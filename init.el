23d37f302c482351ddf7e668aa2e22e42897f101
VeronikaGrishina <95370627+VeronikaGrishina@users.noreply.github.com>
2026-05-19 07:23:53 +0600

    Update init.el
    Removed Custom variables

--- init.el
+++ init.el
@@ -102,23 +102,3 @@
 
 ;; set theme
 ;; (add-hook 'after-init-hook (lambda () (load-theme 'doom-monokai-spectrum)))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(doom-1337))
- '(custom-safe-themes
-   '("014cb63097fc7dbda3edf53eb09802237961cbb4c9e9abd705f23b86511b0a69"
-     default))
- '(org-agenda-files '("~/Documents/Notes/Org/20250101.gpg"))
- '(package-selected-packages
-   '(async company doom-themes habitica org-journal rust-mode
-	   yasnippet-snippets))
- '(package-vc-selected-packages '((helm :url "https://github.com/emacs-helm/helm"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
